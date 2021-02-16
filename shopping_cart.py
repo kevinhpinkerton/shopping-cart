2d40dafb96f18002cb0271e86fceb661681dea30
Kevin Pinkerton <kevinhpinkerton@gmail.com>
2021-02-16 15:24:15 -0500

    Fixed datastore integration. Formatted output.

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,16 +2,15 @@
 
 import os
 from dotenv import load_dotenv
-import datetime
+import datetime as dt
 import sys
-import csv
+import pandas as pd
 
-file_name = "receipts/{date:%Y-%m-%d_%H-%M-%S-%f}.txt".format(date=datetime.datetime.now())
+file_name = "receipts/{date:%Y-%m-%d_%H-%M-%S-%f}.txt".format(date=dt.datetime.now())
 # print(file_name)
 
-with open("data/products.csv", "r") as f:
-        reader = csv.DictReader(f)
-        products = list(reader)
+df = pd.read_csv("data/products.csv")
+products = df.to_dict('records')
 
 [item.update({"price_per":"item"}) for item in products]
 products.append({"id":21, "name": "Organic Bananas", "department": "fruit", "aisle": "fruits", "price": 0.79, "price_per": "pound"})
@@ -20,17 +19,17 @@ products.append({"id":21, "name": "Organic Bananas", "department": "fruit", "ais
 def to_usd(my_price):
     return f"${my_price:,.2f}" 
 
-user_input = str()
-user_input_extra = float()
+user_input_id = str()
+user_input_pounds = float()
 user_products = []
 while True:
-    user_input = input("Please input a product identifier: ").lower()
-    if user_input != 'done':
+    user_input_id = input("Please input a product identifier: ").lower()
+    if user_input_id != 'done':
         try:
-            product = next(item for item in products if item["id"] == int(user_input))
+            product = next(item for item in products if item["id"] == int(user_input_id))
             if product["price_per"] == "pound":
-                user_input_extra = input("Please input the number of pounds: ")
-                product["price"] = product["price"] * float(user_input_extra)
+                user_input_pounds = input("Please input the number of pounds: ")
+                product["price"] = product["price"] * float(user_input_pounds)
             user_products.append(product)
         except:
             print("Invalid integer: Type \"Done\" to finish checking out.") 
@@ -41,15 +40,38 @@ while True:
 # stdoutOrigin=sys.stdout 
 # sys.stdout = open(file_name, "w")
 
+print("---------------------------------")
+
+print("GREEN FOODS GROCERY")
+print("WWW.GREEN-FOODS-GROCERY.COM")
+
+print("---------------------------------")
+
+print("CHECKOUT AT: {date:%Y-%m-%d %I:%M %p}".format(date=dt.datetime.now()))
+
+print("---------------------------------")
+
+print("SELECTED PRODUCTS: ")
+for product in user_products:
+    print(" ... " + product["name"] + " (" + to_usd(product["price"]) + ")")
+
+print("---------------------------------")
+
 subtotal = sum([item["price"] for item in user_products])
-print(to_usd(subtotal))
+print("SUBTOTAL: " + to_usd(subtotal))
 
 load_dotenv()
 tax = subtotal * float(os.getenv("TAX_RATE", default=.0875))
-print(to_usd(tax))
+print("TAX: " + to_usd(tax))
 
 total = subtotal + tax
-print(to_usd(total))
+print("TOTAL: " + to_usd(total))
+
+print("---------------------------------")
+
+print("THANKS, SEE YOU AGAIN SOON!")
+
+print("---------------------------------")
 
 # sys.stdout.close()
 # sys.stdout=stdoutOrigin
